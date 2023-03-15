aabc84c8718f9539647bb6119ef2b12782b859df
Daviana Dwitasari Enka Fornia <113608076+davianadwitsr@users.noreply.github.com>
2023-03-15 22:38:35 +0800

    Add files via upload

---
+++ Postest1/Postest1/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        txtJumlah = New TextBox()
+        btnClear = New Button()
+        btnKonversi = New Button()
+        Label5 = New Label()
+        txtHasil = New TextBox()
+        cbSatuan = New ComboBox()
+        cbKonversi = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(171, 26)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(478, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "PROGRAM KONVERSI SATUAN PANJANG"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(50, 118)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(226, 19)
+        Label2.TabIndex = 1
+        Label2.Text = "Masukan Jumlah Satuan Panjang"' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(50, 184)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(147, 19)
+        Label3.TabIndex = 2
+        Label3.Text = "Pilih Satuan Panjang"' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(50, 246)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(213, 19)
+        Label4.TabIndex = 3
+        Label4.Text = "Pilih Satuan Panjang Konversi"' 
+        ' txtJumlah
+        ' 
+        txtJumlah.Location = New Point(339, 114)
+        txtJumlah.Name = "txtJumlah"
+        txtJumlah.Size = New Size(194, 27)
+        txtJumlah.TabIndex = 4
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Times New Roman", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        btnClear.Location = New Point(634, 145)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(94, 29)
+        btnClear.TabIndex = 7
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnKonversi
+        ' 
+        btnKonversi.Font = New Font("Times New Roman", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        btnKonversi.Location = New Point(634, 217)
+        btnKonversi.Name = "btnKonversi"
+        btnKonversi.Size = New Size(94, 29)
+        btnKonversi.TabIndex = 8
+        btnKonversi.Text = "Konversi"
+        btnKonversi.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(259, 336)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(49, 20)
+        Label5.TabIndex = 9
+        Label5.Text = "Hasil"' 
+        ' txtHasil
+        ' 
+        txtHasil.Location = New Point(339, 333)
+        txtHasil.Name = "txtHasil"
+        txtHasil.Size = New Size(194, 27)
+        txtHasil.TabIndex = 10
+        ' 
+        ' cbSatuan
+        ' 
+        cbSatuan.FormattingEnabled = True
+        cbSatuan.Items.AddRange(New Object() {"Pilih Satuan ", "km", "hm", "dam", "m", "dm", "cm", "mm"})
+        cbSatuan.Location = New Point(339, 184)
+        cbSatuan.Name = "cbSatuan"
+        cbSatuan.Size = New Size(194, 28)
+        cbSatuan.TabIndex = 11
+        ' 
+        ' cbKonversi
+        ' 
+        cbKonversi.FormattingEnabled = True
+        cbKonversi.Items.AddRange(New Object() {"Pilih Satuan Konversi", "km", "hm", "dam", "m", "dm", "cm", "mm"})
+        cbKonversi.Location = New Point(339, 243)
+        cbKonversi.Name = "cbKonversi"
+        cbKonversi.Size = New Size(194, 28)
+        cbKonversi.TabIndex = 12
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(cbKonversi)
+        Controls.Add(cbSatuan)
+        Controls.Add(txtHasil)
+        Controls.Add(Label5)
+        Controls.Add(btnKonversi)
+        Controls.Add(btnClear)
+        Controls.Add(txtJumlah)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtJumlah As TextBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnKonversi As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtHasil As TextBox
+    Friend WithEvents cbSatuan As ComboBox
+    Friend WithEvents cbKonversi As ComboBox
+End Class
---
+++ Postest1/Postest1/Form1.vb
@@ -0,0 +1,168 @@
+﻿Public Class Form1
+    Sub Bersih()
+        txtJumlah.Text = ""
+        txtHasil.Text = ""
+        cbSatuan.Text = ""
+        cbKonversi.Text = ""
+
+        txtHasil.Enabled = False
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Call Bersih()
+    End Sub
+
+    Private Sub btnKonversi_Click(sender As Object, e As EventArgs) Handles btnKonversi.Click
+        Dim inputan, hasil As Double
+        inputan = Val(txtJumlah.Text)
+
+        'km ke satuan konversi
+        If (cbSatuan.Text = "km") Then
+            If (cbKonversi.Text = "hm") Then
+                txtHasil.Text = inputan * 10
+
+            ElseIf (cbKonversi.Text = "dam") Then
+                txtHasil.Text = inputan * 100
+
+            ElseIf (cbKonversi.Text = "m") Then
+                txtHasil.Text = inputan * 1000
+
+            ElseIf (cbKonversi.Text = "dm") Then
+                txtHasil.Text = inputan * 10000
+
+            ElseIf (cbKonversi.Text = "cm") Then
+                txtHasil.Text = inputan * 100000
+
+            ElseIf (cbKonversi.Text = "mm") Then
+                txtHasil.Text = inputan * 1000000
+            End If
+
+            'hm ke satuan konversi
+        ElseIf (cbSatuan.Text = "hm") Then
+            If (cbKonversi.Text = "km") Then
+                txtHasil.Text = inputan / 10
+
+            ElseIf (cbKonversi.Text = "dam") Then
+                txtHasil.Text = inputan * 10
+
+            ElseIf (cbKonversi.Text = "m") Then
+                txtHasil.Text = inputan * 100
+
+            ElseIf (cbKonversi.Text = "dm") Then
+                txtHasil.Text = inputan * 1000
+
+            ElseIf (cbKonversi.Text = "cm") Then
+                txtHasil.Text = inputan * 10000
+
+            ElseIf (cbKonversi.Text = "mm") Then
+                txtHasil.Text = inputan * 100000
+            End If
+
+            'dam ke satuan konversi
+        ElseIf (cbSatuan.Text = "dam") Then
+            If (cbKonversi.Text = "km") Then
+                txtHasil.Text = inputan / 100
+
+            ElseIf (cbKonversi.Text = "hm") Then
+                txtHasil.Text = inputan / 10
+
+            ElseIf (cbKonversi.Text = "m") Then
+                txtHasil.Text = inputan * 10
+
+            ElseIf (cbKonversi.Text = "dm") Then
+                txtHasil.Text = inputan * 100
+
+            ElseIf (cbKonversi.Text = "cm") Then
+                txtHasil.Text = inputan * 1000
+
+            ElseIf (cbKonversi.Text = "mm") Then
+                txtHasil.Text = inputan * 10000
+            End If
+
+            'm ke satuan konversi
+        ElseIf (cbSatuan.Text = "m") Then
+            If (cbKonversi.Text = "km") Then
+                txtHasil.Text = inputan / 1000
+
+            ElseIf (cbKonversi.Text = "hm") Then
+                txtHasil.Text = inputan / 100
+
+            ElseIf (cbKonversi.Text = "dam") Then
+                txtHasil.Text = inputan / 10
+
+            ElseIf (cbKonversi.Text = "dm") Then
+                txtHasil.Text = inputan * 10
+
+            ElseIf (cbKonversi.Text = "cm") Then
+                txtHasil.Text = inputan * 100
+
+            ElseIf (cbKonversi.Text = "mm") Then
+                txtHasil.Text = inputan * 1000
+            End If
+
+            'dm ke satuan konversi
+        ElseIf (cbSatuan.Text = "dm") Then
+            If (cbKonversi.Text = "km") Then
+                txtHasil.Text = inputan / 10000
+
+            ElseIf (cbKonversi.Text = "hm") Then
+                txtHasil.Text = inputan / 1000
+
+            ElseIf (cbKonversi.Text = "dam") Then
+                txtHasil.Text = inputan / 100
+
+            ElseIf (cbKonversi.Text = "m") Then
+                txtHasil.Text = inputan / 10
+
+            ElseIf (cbKonversi.Text = "cm") Then
+                txtHasil.Text = inputan * 10
+
+            ElseIf (cbKonversi.Text = "mm") Then
+                txtHasil.Text = inputan * 100
+            End If
+
+            'cm ke satuan konversi
+        ElseIf (cbSatuan.Text = "cm") Then
+            If (cbKonversi.Text = "km") Then
+                txtHasil.Text = inputan / 100000
+
+            ElseIf (cbKonversi.Text = "hm") Then
+                txtHasil.Text = inputan / 10000
+
+            ElseIf (cbKonversi.Text = "dam") Then
+                txtHasil.Text = inputan / 1000
+
+            ElseIf (cbKonversi.Text = "m") Then
+                txtHasil.Text = inputan / 100
+
+            ElseIf (cbKonversi.Text = "dm") Then
+                txtHasil.Text = inputan / 10
+
+            ElseIf (cbKonversi.Text = "mm") Then
+                txtHasil.Text = inputan * 10
+            End If
+
+            'mm ke satuan konversi
+        ElseIf (cbSatuan.Text = "mm") Then
+            If (cbKonversi.Text = "km") Then
+                txtHasil.Text = inputan / 1000000
+
+            ElseIf (cbKonversi.Text = "hm") Then
+                txtHasil.Text = inputan / 100000
+
+            ElseIf (cbKonversi.Text = "dam") Then
+                txtHasil.Text = inputan / 10000
+
+            ElseIf (cbKonversi.Text = "m") Then
+                txtHasil.Text = inputan / 1000
+
+            ElseIf (cbKonversi.Text = "dm") Then
+                txtHasil.Text = inputan / 100
+
+            ElseIf (cbKonversi.Text = "cm") Then
+                txtHasil.Text = inputan * 10
+            End If
+
+        End If
+    End Sub
+End Class
